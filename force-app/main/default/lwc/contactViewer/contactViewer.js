5822a41758a2865d8bb9d22c5c4745bc5922fd05
shakyashiv2003 <kr.shakyashiv0003@gmail.com>
2026-05-04 15:22:42 +0530

    Next Parent Child Communication Project based on LWC!

--- force-app/main/default/lwc/contactViewer/contactViewer.js
+++ force-app/main/default/lwc/contactViewer/contactViewer.js
@@ -1,5 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
 
 export default class ContactViewer extends LightningElement {
-    
+
+    @api accountId;
+    contacts;
+    error;
+
+    @wire(getContacts, {accountId:'$accountId'}) 
+    conRecords({data, error}){
+        if(data){
+            this.contacts = data;
+            this.error = undefined;
+        }
+        if(error){
+            this.error = error;
+            this.data = undefined;
+        }
+    }
 }
